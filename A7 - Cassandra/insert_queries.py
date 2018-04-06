fe8caf354e1e654a9bc813cb0f7d70902a7e438d
udayraj123 <rhuge123@gmail.com>
2018-04-06 08:23:32 +0530

    hashtag count in prog

--- A7 - Cassandra/insert_queries.py
+++ A7 - Cassandra/insert_queries.py
@@ -1,18 +1,21 @@
 columns={}
-columns['tweets_by_tid']=('author', 'author_id', 'author_profile_image', 'author_screen_name', 'hashtags', 'keywords_processed_list', 'lang', 'like_count', 'location', 'media_list', 'mentions', 'quote_count', 'quoted_source_id', 'reply_count', 'replyto_source_id', 'retweet_count', 'retweet_source_id', 'sentiment', 'tid', 'tweet_text', 'tweet_date', 'tweet_datetime', 'type', 'url_list', 'verified' )
-columns['tweets_by_author']=('author', 'author_id', 'author_screen_name', 'lang', 'location', 'tid', 'tweet_text', 'tweet_date', 'tweet_datetime', 'like_count', 'author_profile_image', 'hashtags', 'keywords_processed_list', 'media_list', 'mentions', 'quote_count', 'quoted_source_id', 'reply_count', 'replyto_source_id', 'retweet_count', 'retweet_source_id', 'sentiment', 'type', 'url_list', 'verified' )
-columns['tids_by_author']=('author', 'author_id', 'author_screen_name', 'tid', 'tweet_date', 'tweet_datetime' )
-columns['tids_by_date']=('tweet_date', 'tweet_datetime', 'tid' )
-#Requires removing nulls- Let's do it general _/
+columns['tweets_by_tid']=('tid','author', 'author_id', 'author_profile_image', 'author_screen_name', 'hashtags', 'keywords_processed_list', 'lang', 'like_count', 'location', 'media_list', 'mentions', 'quote_count', 'quoted_source_id', 'reply_count', 'replyto_source_id', 'retweet_count', 'retweet_source_id', 'sentiment', 'tweet_text', 'tweet_date', 'tweet_datetime', 'type', 'url_list', 'verified' )
+columns['tweets_by_author']=('author_screen_name','author', 'author_id', 'lang', 'location', 'tid', 'tweet_text', 'tweet_date', 'tweet_datetime', 'like_count', 'author_profile_image', 'hashtags', 'keywords_processed_list', 'media_list', 'mentions', 'quote_count', 'quoted_source_id', 'reply_count', 'replyto_source_id', 'retweet_count', 'retweet_source_id', 'sentiment', 'type', 'url_list', 'verified' )
+columns['tids_by_date']=('tweet_date', 'like_count', 'tweet_datetime', 'tid' )
+# Requires Removing nulls: Let's do it general _/
 columns['tids_by_location']=('location', 'tid', 'tweet_date', 'tweet_datetime' ) 
-# # Contains some custom columns-
 
+# # Contains some custom columns-
 columns['tids_by_hashtag']=('hashtag', 'tid', 'tweet_date', 'tweet_datetime' ) 
 columns['tids_by_mention']=('mention', 'tid', 'tweet_date', 'tweet_datetime' ) 
+columns['tids_by_keyword']=('keyword', 'tid', 'like_count' ) 
 
 # Needs completely diff function
-# columns['hashtag_counter']=('hash_week', 'hash_date', 'hashtag', 'hashtag_count' ) 
+columns['popular_hashtags']=( 'hash_date', 'hashtag_count', 'hashtag' ) 
 statements={}
 for table in columns.keys():
 	cols=columns[table]
 	statements[table]="insert into twitterdb."+table+" ("+",".join(cols)+") values (" + "?,"*(len(cols)-1)+"?" + ") if not exists";
+
+
+# columns['tids_by_author']=('author_screen_name','author', 'author_id', 'tid', 'tweet_date', 'tweet_datetime' )
---
+++ A7 - Cassandra/select_queries.php
@@ -0,0 +1,19 @@
+<?php
+$all_cols=['author', 'author_id', 'author_profile_image', 'author_screen_name', 'hashtags', 'keywords_processed_list', 'lang', 'like_count', 'location', 'media_list', 'mentions', 'quote_count', 'quoted_source_id', 'reply_count', 'replyto_source_id', 'retweet_count', 'retweet_source_id', 'sentiment', 'tid', 'tweet_text', 'tweet_date', 'tweet_datetime', 'type', 'url_list', 'verified' ];
+$normal_cols_str='author_screen_name,author,tid,tweet_text,location,lang,tweet_datetime';
+return array(
+'query00'=> "select $normal_cols_str from twitterdb.tweets_by_tid where tid = ?",
+'query0'=> "select $normal_cols_str from twitterdb.tweets_by_tid where tid = ?",
+
+'query1'=> "select $normal_cols_str from twitterdb.tweets_by_author where author_screen_name = ?",
+'query2'=> "select keyword,like_count,tid from twitterdb.tids_by_keyword where keyword = ?",
+// 'query2'=> "select $normal_cols_str from twitterdb.tids_by_keyword where keyword = ?",
+'query3'=> "select $normal_cols_str from twitterdb.tids_by_hashtag where hashtag = ?",
+'query4'=> "select $normal_cols_str from twitterdb.tids_by_mention where mention = ?",
+// 'query5'=> "select $normal_cols_str from twitterdb.tids_by_date where tweet_date = ?",
+'query5'=> "select tweet_date,tweet_datetime,tid from twitterdb.tids_by_date where tweet_date = ?",
+'query6'=> "select $normal_cols_str from twitterdb.tids_by_location where location = ?",
+'query7'=> "select $normal_cols_str from twitterdb.popular_hashtags where counter = ?",
+);
+
+?>
